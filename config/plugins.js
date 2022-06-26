5d8d4cf7afeb7efe398474390a05ade097a2439b
Romulo Roriz <romulonmroriz@gmail.com>
2022-06-26 11:20:06 +0200

    Add Strapi memory cache

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,25 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 3600,
+        },
+      },
+      strategy: {
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::category.category",
+          "api::article.article",
+          "api::global.global",
+          "api::homepage.homepage",
+          "api::writer.writer",
+        ],
+      },
+    },
+  },
 });
